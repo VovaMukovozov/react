e15bb4fc7e4d71694b8e0e0dff623322c3c5e02e
vova_mukovozov <vova.mukovozov@sergata.com>
2016-02-24 16:27:54 +0200

    added jsx

---
+++ app/main.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+
+console.log('Hello iam JSX file! ');
+
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+React.render(<GroceryItemList />, app);
