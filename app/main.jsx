6699a9729afc63cc60b5465fc337e586e8231336
vova_mukovozov <vova.mukovozov@sergata.com>
2016-02-27 23:41:18 +0200

    added routing

--- app/main.jsx
+++ app/main.jsx
@@ -1,22 +1,72 @@
-var React = require('react');
-var ReactDom = require('react-dom');
+var React = require('react'),
+    ReactRouter = require('react-router'),
+    ReactDom = require('react-dom'),
+    App = require('./components/app.jsx'),
+    Home = require('./components/home.jsx'),
+    Login = require('./components/Login/login.jsx'),
+    Logout = require('./components/Login/logout.jsx'),
+    Dashboard = require('./components/dashboard.jsx'),
+    Router = ReactRouter.Router,
+    Route = ReactRouter.Route,
+    browserHistory = ReactRouter.hashHistory,
+    IndexRoute = ReactRouter.IndexRoute;
 
-console.log('Hello iam JSX file! ');
+var routes = (
+        <Route component={App} path='/'>
+            <IndexRoute component={Home} />
+            <Route path="login" component={Login}/>
+            <Route path="logout" component={Logout}/>
+            <Route path="dashboard" component={Dashboard}/>
+        </Route>
+);
 
-var GroceryItemList = require('./components/GroceryItem/GroceryItemList.jsx');
-
-var GroceryItemStore = require('./stores/GroceryItemStore.jsx');
-
-var initial = GroceryItemStore.getItems();
+ReactDom.render(<Router history={browserHistory} >{routes}</Router>,   app)
 
 
-function render(){
-    ReactDom.render(<GroceryItemList items={initial}/>, app);
-}
 
-GroceryItemStore.onChange(function(items){
-    initial = items;
-    render();
-});
 
-render();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var React = require('react');
+//var ReactDom = require('react-dom');
+//
+//console.log('Hello iam JSX file! ');
+//
+//var GroceryItemList = require('./components/GroceryItem/GroceryItemList.jsx');
+//
+//var GroceryItemStore = require('./stores/GroceryItemStore.jsx');
+//
+//var initial = GroceryItemStore.getItems();
+//
+//
+//function render(){
+//    ReactDom.render(<GroceryItemList items={initial}/>, app);
+//}
+//
+//GroceryItemStore.onChange(function(items){
+//    initial = items;
+//    render();
+//});
+//
+//render();
