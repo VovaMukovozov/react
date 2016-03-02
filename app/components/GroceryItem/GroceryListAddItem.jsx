0877f3a9a9a43555a86880415448694993582a1b
vova_mukovozov <vova.mukovozov@sergata.com>
2016-03-02 18:06:55 +0200

    add modal

--- app/components/GroceryItem/GroceryListAddItem.jsx
+++ app/components/GroceryItem/GroceryListAddItem.jsx
@@ -1,12 +1,25 @@
 var React = require('react'),
     InputForm = require('./../General/FormInput.jsx'),
-    Action = require('./../../actions/CroceryItemActionCeator.jsx');
+    Action = require('./../../actions/CroceryItemActionCeator.jsx'),
+    Modal = require('react-modal');
+
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : '50%',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
+};
 
 module.exports = React.createClass({
 
     getInitialState: function(){
         return {
-            canSubmit: false
+            canSubmit: false,
+            modalIsOpen: false
         }
     },
 
@@ -15,6 +28,7 @@ module.exports = React.createClass({
             canSubmit: true
         });
     },
+
     disableButton: function () {
         this.setState({
             canSubmit: false
@@ -28,15 +42,36 @@ module.exports = React.createClass({
         });
 
         this.refs.form.reset();
+        this.closeModal();
     },
-    render: function(){
+
+    openModal: function() {
+        this.setState({modalIsOpen: true});
+    },
+
+    closeModal: function() {
+        this.setState({modalIsOpen: false});
+    },
+
+    render: function() {
         return (
-            <div className="grocery-addItem">
-                <Formsy.Form ref="form" onValidSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton}>
-                    <InputForm name="item" inputClassName="u-full-width" validations="isExisty" validationError="This is not a valid item" required/>
-                    <button type="submit" disabled={!this.state.canSubmit} >Add Item</button>
-                </Formsy.Form>
+            <div>
+                <button onClick={this.openModal}>Add Item</button>
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onRequestClose={this.closeModal}
+                    style={customStyles} >
+
+                    <div className="grocery-addItem">
+                        <Formsy.Form ref="form" onValidSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton}>
+                         <InputForm name="item" inputClassName="u-full-width" validations="isExisty" validationError="This is not a valid item" required/>
+                            <button type="submit" disabled={!this.state.canSubmit} >Add Item</button>
+                            <button onClick={this.closeModal}>close</button>
+                        </Formsy.Form>
+                    </div>
+                </Modal>
             </div>
-        )
+        );
     }
+
 });
