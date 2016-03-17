6a1c626f434c963b786c94b5151fb9dc59a62746
vova.mukovozov.sergata <vova.mukovozov@sergata.com>
2016-03-17 22:12:35 +0200

    adding ajax sending

--- app/components/dashboard.jsx
+++ app/components/dashboard.jsx
@@ -1,9 +1,8 @@
 var React = require('react'),
     auth = require('../services/auth.js');
-
 var GroceryItemStore = require('../stores/GroceryItemStore.jsx');
 var GroceryItemList = require('../components/GroceryItem/GroceryItemList.jsx');
-var initial = GroceryItemStore.getItems();
+var initial = [];
 
 var Dashboard = React.createClass({
 
@@ -12,7 +11,22 @@ var Dashboard = React.createClass({
             <GroceryItemList items={initial}/>
         );
     },
+
+
     componentDidMount: function () {
+
+    fetch( 'http://localhost:8080/notes', {
+                method: 'get',
+                headers: {
+                          "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6Niwia2V5cyI6IkEiLCJzZWNyZXQiOiJNVDJMcTM5YUcsRTRVOXc9NCNDWW9mZnJKKks4KzN6KHhkUFljO2RaZ1Fwb1hHVCg4W0AxMyQlZGpsekA9QnYifQ.pBkIe3VYOqFcOrc7kQg-rBYVcL7RbSekiST1Zz09E6E"
+                          },
+        })
+        .then(function (data) {
+            console.log('Request succeeded with JSON response', data);
+            })
+  .catch(function (error) {
+    console.log('Request failed', error);
+  });
         GroceryItemStore.onChange(function(items){
             initial = items;
             this.setState({ items: initial });
